024f2bc440c2a272581a998fe20ebcf4f3e99c06
Clémence Le Roux <clemence.le-roux@ecole.ensicaen.fr>
2023-01-11 15:32:43 +0100

    adding last route warning TODO : parse post results

--- conso_score_back/data_access/insert_datas.py
+++ conso_score_back/data_access/insert_datas.py
@@ -10,10 +10,54 @@ def register_maker_product_db(maker_id,name, price, carbon_foot_print, quantity_
         product_id = cursor.fetchone()
 
         cursor.execute("insert into public.maker_stock \
-                        (maker_id, product_id) values (%s,%s)",(maker_id, product_id))
+                        (maker_id, product_id) values (%s,%s)",(maker_id, product_id[0]))
         get_db().commit()
     except Exception as e:
         print(e)
         return "an error occured while registering the product"
        
+def register_seller_product_db(seller_id, product_id, price, bar_code,quantity):
+    try: 
+        cursor = get_db().cursor()
+        #fetch seller_stock_id
+        cursor.execute("SELECT DISTINCT seller_stock_id FROM seller_stock\
+                        JOIN seller ON seller.seller_id=seller_stock.seller_id\
+                        WHERE seller.seller_id=%s",(seller_id,))
+        seller_stock_id = cursor.fetchone()
 
+        conso_score, tax = calculate_conso_score(product_id, quantity)
+        if tax == -1:
+            return  {"conso_score":-1, "tax":-1}#an error occured 
+
+        #insert seller_product
+        cursor.execute("insert into public.seller_product\
+            ( product_id, price, conso_score, bar_code, tax)\
+            values (%s,%s, %s,%s, %s) RETURNING seller_product_id",(product_id, price, conso_score, bar_code, tax))
+        seller_product_id = cursor.fetchone()
+
+        if seller_stock_id == None:
+            cursor.execute("insert into public.seller_stock\
+                (seller_product_id, seller_id, quantity)\
+                values (%s,%s, %s)",(seller_product_id[0], seller_id, quantity))
+        else:
+            cursor.execute("insert into public.seller_stock\
+                (seller_stock_id, seller_product_id, seller_id, quantity)\
+                values (%s, %s,%s, %s)",(seller_stock_id[0], seller_product_id[0], seller_id, quantity))
+
+            get_db().commit()
+            return {"conso_score":conso_score, "tax":tax}
+    except Exception as e:
+        print(e)
+        return "an error occured while registering the seller product"
+
+def calculate_conso_score(product_id, quantity):
+    try : 
+        cursor = get_db().cursor()
+        cursor.execute("SELECT carbon_footprint FROM product WHERE product_id=%s",(product_id,))
+        carbon_footprint = cursor.fetchone()[0]
+        conso_score = carbon_footprint * quantity / 100 #TOTO ADAPTER
+        taxe = conso_score * 0.05
+        return conso_score, taxe
+    except Exception as e:
+        print(e)
+        return "an error occured while calculating the conso score", -1
--- conso_score_back/paths/seller.py
+++ conso_score_back/paths/seller.py
@@ -2,6 +2,7 @@ from flask import Blueprint, request, jsonify, request,render_template, flash, s
 from flask_cors import cross_origin
 from ..data_access.get_seller_datas import  get_seller_partners_db
 from ..data_access.get_datas import get_all_transports_db, get_all_products_db
+from ..data_access.insert_datas import register_seller_product_db
 bpapi = Blueprint('/seller', __name__, url_prefix='/seller')
 
 @bpapi.route("/")
@@ -11,13 +12,20 @@ def home():
 
 @bpapi.route("/<seller_id>/product", methods=['GET','POST'])
 def seller_get_product(seller_id):
-    #todo parse json request response and fill the var
-    bar_code = 123
-    maker_product_id = "abc-er"
-    price = 2
-    quantity = 8
-    #call get_product_score(bar_code,maker_product_id, price,quantity)
-    return {"conso_score":0, "tax":0} #todo
+    if request.method == 'GET':
+        #todo parse json request response and fill the var
+        seller_id = '5a39c805-4e15-4944-80af-333d2d11bced'
+        product_id = '87a1e36f-c193-4640-ba98-fd8dd58e25ce'
+        price = 8
+        bar_code = 456
+        quantity = 5
+        return register_seller_product_db(seller_id, product_id, price, bar_code,quantity)
+        
+    elif request.method == 'POST':
+        #todo parse json request response and fill the var
+
+        #call get_product_score(bar_code,maker_product_id, price,quantity)
+        return {"conso_score":0, "tax":0}
 
 @bpapi.route("/<seller_id>/makers", methods=['GET'])
 def seller_partners(seller_id):
