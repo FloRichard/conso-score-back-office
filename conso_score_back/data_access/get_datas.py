a32330c02848585ead8da21edac8ab73c0e69ed8
Clémence Le Roux <clemence.le-roux@ecole.ensicaen.fr>
2023-01-11 16:29:31 +0100

    add mode info on datas/product route

--- conso_score_back/data_access/get_datas.py
+++ conso_score_back/data_access/get_datas.py
@@ -18,10 +18,23 @@ def get_all_transports_db():
 def get_product_by_barcode(bar_code):
     try:
         cursor = get_db().cursor()
-        cursor.execute("SELECT * FROM seller_product\
-                        WHERE seller_product.bar_code=%s",(bar_code,))
+        cursor.execute("SELECT * FROM seller_product WHERE seller_product.bar_code=%s",(bar_code,))
         result = cursor.fetchone()
         product = {"product_id":result[1],"price":result[2],"conso_score":result[3], "bar_code":result[4], "tax":result[5]}
+        cursor.execute("SELECT * FROM product WHERE product_id = %s", (product['product_id'],))
+        result = cursor.fetchone()
+        product["name"] = result[1]
+        product["carbon_foot_print"] = result[3]
+        product["quantity_unity"] = result[4]
+        category = result[5]
+        transport_cat = result[6]
+        print("product", product)
+        cursor.execute("SELECT name FROM category WHERE category_id = %s", (category,))
+        product["category"] = cursor.fetchone()[0]
+
+        cursor.execute("SELECT name FROM expedition_transport WHERE expedition_transport_id = %s", (transport_cat,))
+        product["transport"] = cursor.fetchone()[0]
+        print(product)
         return product
     except Exception as e:
         print(e)
